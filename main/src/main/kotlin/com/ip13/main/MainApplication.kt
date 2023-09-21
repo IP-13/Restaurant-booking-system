b07f2bb22e41c7f30b2c992a877206560bb6fab7
IP-13 <iperevozchikov2002@gmail.com>
2023-09-21 19:39:23 +0300

    first commit

---
+++ main/src/main/kotlin/com/ip13/main/MainApplication.kt
@@ -0,0 +1,11 @@
+package com.ip13.main
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MainApplication
+
+fun main(args: Array<String>) {
+	runApplication<MainApplication>(*args)
+}
---
+++ main/src/test/kotlin/com/ip13/main/MainApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ip13.main
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
