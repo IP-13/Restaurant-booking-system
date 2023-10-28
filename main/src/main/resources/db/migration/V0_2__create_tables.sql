285c5e85200574e733ba303ce125f03dc8bc7bd1
IP-13 <iperevozchikov2002@gmail.com>
2023-10-28 11:16:57 +0300

    add_restaurant_ticket schema changed

--- main/src/main/resources/db/migration/V0_2__create_tables.sql
+++ main/src/main/resources/db/migration/V0_2__create_tables.sql
@@ -1,36 +1,14 @@
-create table if not exists address (
-    id int generated always as identity(start with 100 increment by 100) primary key,
-    country text not null,
-    city text not null,
-    street text not null,
-    building int not null,
-    entrance int,
-    floor int
-);
-
-create table if not exists restaurant (
-    id int generated always as identity(start with 100 increment by 100) primary key,
-    name varchar(50) not null,
-    address_id int references address(id),
-    description text
-);
-
 create table if not exists user_t (
     id int generated always as identity(start with 100 increment by 100) primary key,
     username varchar(50) not null,
-    password text not null
+    password text not null,
+    rating float,
 );
 
 create table if not exists user_role (
     id int generated always as identity(start with 100 increment by 100) primary key,
     user_id int references user_t(id),
     role role
-);
-
-create table if not exists visitor (
-    id int generated always as identity(start with 100 increment by 100) primary key,
-    user_id int references user_t(id),
-    rating float
 );
 
 create table if not exists admin (
@@ -68,15 +46,41 @@ create table if not exists booking_constraint (
 
 create table if not exists add_restaurant_ticket (
     id int generated always as identity(start with 100 increment by 100) primary key,
-    manager_id int references manager(id),
+    restaurant_name varchar(50) not null,
+    country text not null,
+    city text not null,
+    street text not null,
+    building int not null,
+    entrance int,
+    floor int,
+    description text,
+    user_id int references user_t(id),
     admin_id int references admin(id),
-    restaurant_id int references restaurant(id),
-    status reserve_table_status,
+    status add_restaurant_status,
     create_date timestamp not null,
     last_status_update timestamp not null,
     author_comment text,
     admin_comment text
 );
+
+create table if not exists address (
+    id int generated always as identity(start with 100 increment by 100) primary key,
+    country text not null,
+    city text not null,
+    street text not null,
+    building int not null,
+    entrance int,
+    floor int
+);
+
+create table if not exists restaurant (
+    id int generated always as identity(start with 100 increment by 100) primary key,
+    name varchar(50) not null,
+    address_id int references address(id),
+    add_restaurant_ticket_id int references add_restaurant_ticket(id),
+    description text
+);
+
 
 create table if not exists black_list (
     id int generated always as identity(start with 100 increment by 100) primary key,
