2072ab15e714482c6a638b203898666d6608c3ca
IP-13 <iperevozchikov2002@gmail.com>
2023-10-30 12:34:39 +0300

    logger added

--- main/src/main/kotlin/com/ip13/main/controller/AdminController.kt
+++ main/src/main/kotlin/com/ip13/main/controller/AdminController.kt
@@ -3,6 +3,7 @@ package com.ip13.main.controller
 import com.ip13.main.model.dto.RoleAddDto
 import com.ip13.main.model.entity.enums.Role
 import com.ip13.main.security.service.UserService
+import com.ip13.main.util.getLogger
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
@@ -13,21 +14,28 @@ import org.springframework.web.bind.annotation.*
 class AdminController(
     val userService: UserService,
 ) {
+    private val logger = getLogger(javaClass)
+
     @PostMapping("/add_role")
     fun addRole(
         @RequestBody(required = true)
         roleAddDto: RoleAddDto,
     ): ResponseEntity<*> {
+        logger.debug("/admin/add_role endpoint invoked")
+
         // TODO() параметр принимается строкой, преобразуется в enum, проверяется, преобразуется обратно в строку
         val role = try {
             Role.valueOf(roleAddDto.role)
         } catch (ex: IllegalArgumentException) {
-            return ResponseEntity("No such role: ${roleAddDto.role}", HttpStatus.BAD_REQUEST)
+            logger.debug("Role \"${roleAddDto.role}\" not found")
+            return ResponseEntity("No such role \"${roleAddDto.role}\"", HttpStatus.BAD_REQUEST)
         }
 
         // TODO() is it ok?
         userService.checkUser(roleAddDto.userId)
 
+        // TODO() сначала чекаем юзера, потом роли этого юзера. Первая проверка юзера выглядит необязательной,
+        // но без нее не получается отдать пользователю правильный ответ
         val isAlreadyHasRole = userService.checkRole(roleAddDto.userId, role.code)
 
         return if (isAlreadyHasRole) {
