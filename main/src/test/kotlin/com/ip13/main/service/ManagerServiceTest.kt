cda4f002d4e18a1c3a1a122b20798be85097fd65
IP-13 <iperevozchikov2002@gmail.com>
2023-11-03 11:11:31 +0300

    ManagerServiceTest added

---
+++ main/src/test/kotlin/com/ip13/main/service/ManagerServiceTest.kt
@@ -0,0 +1,29 @@
+package com.ip13.main.service
+
+import com.ip13.main.model.entity.Manager
+import com.ip13.main.repository.ManagerRepository
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MockKExtension::class)
+class ManagerServiceTest {
+    @MockK
+    private lateinit var managerRepository: ManagerRepository
+
+    @InjectMockKs
+    private lateinit var managerService: ManagerService
+
+    @Test
+    fun saveTest() {
+        val manager = Manager(id = 13)
+
+        every { managerRepository.save(any()) } returns manager
+
+        Assertions.assertThat(managerService.save(manager)).isEqualTo(13)
+    }
+}
