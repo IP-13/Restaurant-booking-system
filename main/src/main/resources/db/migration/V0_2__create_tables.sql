65ab100e9c0e80e9be719fed88c77c75bdf27467
IP-13 <iperevozchikov2002@gmail.com>
2023-11-04 09:46:23 +0300

    num_of_grades and sum_of_grades added to user instead rating

--- main/src/main/resources/db/migration/V0_2__create_tables.sql
+++ main/src/main/resources/db/migration/V0_2__create_tables.sql
@@ -2,7 +2,8 @@ create table if not exists user_t (
     id int generated always as identity(start with 100 increment by 100) primary key,
     username varchar(50) not null,
     password text not null,
-    rating float,
+    num_of_grades int not null,
+    sum_of_grades int not null,
     roles text[]
 );
 
