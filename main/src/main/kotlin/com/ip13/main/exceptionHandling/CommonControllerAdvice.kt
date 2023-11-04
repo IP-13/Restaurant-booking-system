49204eb5e72324668a25640877c44be02af5c14a
IP-13 <iperevozchikov2002@gmail.com>
2023-11-04 22:13:34 +0300

    ResponseStatusException handling added to ControllerAdvice

--- main/src/main/kotlin/com/ip13/main/exceptionHandling/CommonControllerAdvice.kt
+++ main/src/main/kotlin/com/ip13/main/exceptionHandling/CommonControllerAdvice.kt
@@ -3,9 +3,11 @@ package com.ip13.main.exceptionHandling
 import com.ip13.main.exceptionHandling.dto.CommonResponse
 import com.ip13.main.exceptionHandling.exception.UserNotFoundException
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.server.ResponseStatusException
 import java.time.LocalDateTime
 
 @ControllerAdvice
@@ -14,5 +16,10 @@ class CommonControllerAdvice {
     fun handleUserNotFoundException(ex: UserNotFoundException): ResponseEntity<CommonResponse> {
         val message = "${LocalDateTime.now()}: $ex.message"
         return ResponseEntity(CommonResponse(message), HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(ResponseStatusException::class)
+    fun handleResponseStatusException(ex: ResponseStatusException): ResponseEntity<CommonResponse> {
+        return ResponseEntity(CommonResponse(message = ex.message), ex.statusCode)
     }
 }
