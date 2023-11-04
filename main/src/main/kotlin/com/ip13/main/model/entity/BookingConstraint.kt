727d9a98bbb18a0f9246f9aa7d75df81b0b7d643
IP-13 <iperevozchikov2002@gmail.com>
2023-11-04 11:23:51 +0300

    BookingConstraint entity updated

--- main/src/main/kotlin/com/ip13/main/model/entity/BookingConstraint.kt
+++ main/src/main/kotlin/com/ip13/main/model/entity/BookingConstraint.kt
@@ -1,6 +1,9 @@
 package com.ip13.main.model.entity
 
-import jakarta.persistence.*
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 import java.time.LocalDateTime
 
 @Entity
@@ -8,15 +11,9 @@ class BookingConstraint(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Int = 0,
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "restaurant_id")
-    val restaurant: Restaurant = Restaurant(),
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "manager_id")
-    val manager: Manager = Manager(),
-    val reason: String = "",
-    @Column(name = "creation_date")
-    val creationDate: LocalDateTime = LocalDateTime.now(),
-    @Column(name = "expiration_date")
-    val expirationDate: LocalDateTime = LocalDateTime.now(),
+    val restaurantId: Int = 0,
+    val managerId: Int = 0,
+    val reason: String? = null,
+    val fromDate: LocalDateTime = LocalDateTime.now(),
+    val tillDate: LocalDateTime = LocalDateTime.now(),
 )
