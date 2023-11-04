fc7fbe321b4b085d7dd1db807e89f14fe89e57ea
IP-13 <iperevozchikov2002@gmail.com>
2023-11-04 20:31:52 +0300

    username is unique

--- main/src/main/resources/db/migration/V0_2__create_tables.sql
+++ main/src/main/resources/db/migration/V0_2__create_tables.sql
@@ -1,6 +1,6 @@
 create table if not exists user_t (
     id int generated always as identity(start with 100 increment by 100) primary key,
-    username varchar(50) not null,
+    username varchar(50) not null unique,
     password text not null,
     num_of_grades int not null,
     sum_of_grades int not null,
