58c15f83e32ce133e250e097a8185ff1238b51cb
IP-13 <iperevozchikov2002@gmail.com>
2023-11-04 11:08:11 +0300

    TableReserveTicket updated

--- main/src/main/kotlin/com/ip13/main/model/entity/TableReserveTicket.kt
+++ main/src/main/kotlin/com/ip13/main/model/entity/TableReserveTicket.kt
@@ -1,7 +1,9 @@
 package com.ip13.main.model.entity
 
-import com.ip13.main.security.entity.User
-import jakarta.persistence.*
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 import java.time.LocalDateTime
 
 @Entity
@@ -9,12 +11,11 @@ class TableReserveTicket(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Int = 0,
-    @ManyToOne
-    @JoinColumn(name = "restaurant_id")
-    val restaurant: Restaurant = Restaurant(),
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    val user: User = User(),
+    val restaurantId: Int = 0,
+    val userId: Int = 0,
     val creationDate: LocalDateTime = LocalDateTime.now(),
+    val fromDate: LocalDateTime = LocalDateTime.now(),
+    val tillDate: LocalDateTime = LocalDateTime.now(),
+    val numOfGuests: Int = 0,
     val userComment: String? = null,
 )
