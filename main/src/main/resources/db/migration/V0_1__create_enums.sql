bd2d62d09c7c313a6f351a2bd8eea85adf59dfa6
IP-13 <iperevozchikov2002@gmail.com>
2023-11-04 14:29:03 +0300

    small fix

--- main/src/main/resources/db/migration/V0_1__create_enums.sql
+++ main/src/main/resources/db/migration/V0_1__create_enums.sql
@@ -4,4 +4,4 @@ create type restaurant_add_status as enum ('PROCESSING', 'ACCEPTED', 'REJECTED')
 
 create cast (character varying as role) with inout as implicit;
 create cast (character varying as reserve_table_status) with inout as implicit;
-create cast (character varying as restaurant_add_result) with inout as implicit;
+create cast (character varying as restaurant_add_status) with inout as implicit;
