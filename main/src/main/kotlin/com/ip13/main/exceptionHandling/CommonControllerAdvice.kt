105d44bc4375d4f5ec625a96604dd5021678d88d
IP-13 <iperevozchikov2002@gmail.com>
2023-11-05 17:27:26 +0300

    NotFoundException added

--- main/src/main/kotlin/com/ip13/main/exceptionHandling/CommonControllerAdvice.kt
+++ main/src/main/kotlin/com/ip13/main/exceptionHandling/CommonControllerAdvice.kt
@@ -1,9 +1,8 @@
 package com.ip13.main.exceptionHandling
 
 import com.ip13.main.exceptionHandling.dto.CommonResponse
-import com.ip13.main.exceptionHandling.exception.UserNotFoundException
+import com.ip13.main.exceptionHandling.exception.NotFoundException
 import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -12,8 +11,8 @@ import java.time.LocalDateTime
 
 @ControllerAdvice
 class CommonControllerAdvice {
-    @ExceptionHandler(UserNotFoundException::class)
-    fun handleUserNotFoundException(ex: UserNotFoundException): ResponseEntity<CommonResponse> {
+    @ExceptionHandler(NotFoundException::class)
+    fun handleUserNotFoundException(ex: NotFoundException): ResponseEntity<CommonResponse> {
         val message = "${LocalDateTime.now()}: $ex.message"
         return ResponseEntity(CommonResponse(message), HttpStatus.BAD_REQUEST)
     }
