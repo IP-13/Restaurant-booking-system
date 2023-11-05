b3c565fdefdeae4daaa43f7aac25dbed201a7587
IP-13 <iperevozchikov2002@gmail.com>
2023-11-05 17:27:59 +0300

    NotFoundException implemented

--- main/src/main/kotlin/com/ip13/main/exceptionHandling/exception/UserNotFoundException.kt
+++ main/src/main/kotlin/com/ip13/main/exceptionHandling/exception/UserNotFoundException.kt
@@ -2,4 +2,4 @@ package com.ip13.main.exceptionHandling.exception
 
 class UserNotFoundException(
     override val message: String = "User not found",
-) : Exception()
+) : NotFoundException(message)
