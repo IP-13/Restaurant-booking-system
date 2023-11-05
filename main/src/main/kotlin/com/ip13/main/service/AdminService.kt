0b34d32b5936bce6e49286d39690466642220530
IP-13 <iperevozchikov2002@gmail.com>
2023-11-05 18:19:29 +0300

    Service annotation added to AdminService

--- main/src/main/kotlin/com/ip13/main/service/AdminService.kt
+++ main/src/main/kotlin/com/ip13/main/service/AdminService.kt
@@ -2,7 +2,9 @@ package com.ip13.main.service
 
 import com.ip13.main.model.entity.Admin
 import com.ip13.main.repository.AdminRepository
+import org.springframework.stereotype.Service
 
+@Service
 class AdminService(
     private val adminRepository: AdminRepository
 ) {
