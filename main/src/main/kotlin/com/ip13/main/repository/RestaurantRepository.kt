ca9dc5dd9070bd7944b7573449a2834c8e02958f
IP-13 <iperevozchikov2002@gmail.com>
2023-11-06 22:46:48 +0300

    addGrade added to restaurantRepository

--- main/src/main/kotlin/com/ip13/main/repository/RestaurantRepository.kt
+++ main/src/main/kotlin/com/ip13/main/repository/RestaurantRepository.kt
@@ -1,8 +1,26 @@
 package com.ip13.main.repository
 
 import com.ip13.main.model.entity.Restaurant
+import jakarta.transaction.Transactional
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
-interface RestaurantRepository: CrudRepository<Restaurant, Int>
+interface RestaurantRepository : CrudRepository<Restaurant, Int> {
+    @Transactional
+    @Modifying
+    @Query(
+        "update restaurant set num_of_grades = num_of_grades + 1, sum_of_grades = sum_of_grades + :grade " +
+                "where restaurant_id = :restaurant_id",
+        nativeQuery = true
+    )
+    fun addGrade(
+        @Param("restaurant_id")
+        restaurantId: Int,
+        @Param("grade")
+        grade: Int
+    ): Int
+}
