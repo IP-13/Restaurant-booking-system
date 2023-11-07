36895bdf9d0f2dc285f171a5d3904f8b7cdb6db5
IP-13 <iperevozchikov2002@gmail.com>
2023-11-07 23:12:52 +0300

    db schema changed

--- main/src/main/resources/db/migration/V0_2__create_tables.sql
+++ main/src/main/resources/db/migration/V0_2__create_tables.sql
@@ -7,55 +7,38 @@ create table if not exists user_t (
     roles text[]
 );
 
-create table if not exists admin (
+create table if not exists restaurant_add_ticket (
     id int generated always as identity(start with 100 increment by 100) primary key,
-    user_id int references user_t(id),
-    is_active boolean
-);
-
-create table if not exists address (
-    id int generated always as identity(start with 100 increment by 100) primary key,
+    name varchar(50) not null,
     country text not null,
     city text not null,
     street text not null,
     building int not null,
     entrance int,
-    floor int
-);
-
-create table if not exists restaurant_add_ticket (
-    id int generated always as identity(start with 100 increment by 100) primary key,
-    name varchar(50) not null,
-    address_id int references address(id),
+    floor int,
     description text,
     user_id int references user_t(id),
-    creation_date timestamp not null
-);
-
-create table if not exists restaurant_add_ticket_result (
-    id int generated always as identity(start with 100 increment by 100) primary key,
-    restaurant_add_ticket_id int references restaurant_add_ticket(id),
-    admin_id int references admin(id),
-    result restaurant_add_status,
     creation_date timestamp not null,
+    result restaurant_add_status,
+    admin_id int references user_t(id),
+    processing_date timestamp,
     admin_comment text
 );
 
 create table if not exists restaurant (
     id int generated always as identity(start with 100 increment by 100) primary key,
-    name varchar(50) not null,
-    address_id int references address(id),
     restaurant_add_ticket_id int references restaurant_add_ticket(id),
+    manager_id int references user_t(id),
+    name varchar(50) not null,
+    country text not null,
+    city text not null,
+    street text not null,
+    building int not null,
+    entrance int,
+    floor int,
     description text,
     num_of_grades int not null,
     sum_of_grades int not null
-);
-
-create table if not exists manager (
-    id int generated always as identity(start with 100 increment by 100) primary key,
-    user_id int references user_t(id),
-    restaurant_id int references restaurant(id),
-    is_active boolean
 );
 
 create table if not exists table_reserve_ticket (
@@ -70,7 +53,7 @@ create table if not exists table_reserve_ticket (
     till_date timestamp not null,
     num_of_guests int not null,
     user_comment text,
-    manager_id int references manager(id),
+    manager_id int references user_t(id),
     manager_comment text,
     status reserve_table_status
 );
@@ -79,18 +62,18 @@ create table if not exists grade_visitor (
     id int generated always as identity(start with 100 increment by 100) primary key,
     user_id int references user_t(id),
     table_reserve_ticket_id int references table_reserve_ticket(id),
+    -- ресторан, которому пользователь ставит оценку
     restaurant_id int references restaurant(id),
-    -- оценка, которую пользователь поставил ресторану после визита
     grade int not null,
     comment text
 );
 
 create table if not exists grade_manager (
     id int generated always as identity(start with 100 increment by 100) primary key,
-    manager_id int references manager(id),
+    manager_id int references user_t(id),
     table_reserve_ticket_id int references table_reserve_ticket(id),
+    -- пользователь, которому менеджер ставит оценку
     user_id int references user_t(id),
-    -- оценка, которую менеджер поставил пользователю после визита
     grade int not null,
     comment text
 );
@@ -98,7 +81,7 @@ create table if not exists grade_manager (
 create table if not exists booking_constraint (
     id int generated always as identity(start with 100 increment by 100) primary key,
     restaurant_id int references restaurant(id),
-    manager_id int references manager(id),
+    manager_id int references user_t(id),
     reason text not null,
     from_date timestamp not null,
     till_date timestamp not null
