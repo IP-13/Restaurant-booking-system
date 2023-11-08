f961d08b4058a6a0437a46bb3425891c9f0f9380
IP-13 <iperevozchikov2002@gmail.com>
2023-11-08 15:31:39 +0300

    UserRepository and UserService refactoring

--- main/src/main/kotlin/com/ip13/main/security/repository/UserRepository.kt
+++ main/src/main/kotlin/com/ip13/main/security/repository/UserRepository.kt
@@ -1,80 +1,12 @@
 package com.ip13.main.security.repository
 
 import com.ip13.main.security.entity.User
-import jakarta.transaction.Transactional
-import org.springframework.data.jpa.repository.Modifying
-import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
-import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
-import java.time.LocalDateTime
 
 @Repository
 interface UserRepository : CrudRepository<User, Int> {
     fun findByUsername(username: String): User?
 
     fun existsByUsername(username: String): Boolean
-
-    @Modifying
-    @Transactional
-    @Query("update user_t set roles = array_append(roles, :role) where user_t.id = :id", nativeQuery = true)
-    fun addRole(
-        @Param(value = "id")
-        userId: Int,
-        @Param(value = "role")
-        newRole: String
-    ): Int
-
-    @Query("select (select count(*) from user_t where user_t.id = :id and :role = ANY(roles)) > 0", nativeQuery = true)
-    fun checkRole(
-        @Param(value = "id")
-        userId: Int,
-        @Param(value = "role")
-        role: String
-    ): Boolean
-
-    @Query("select (select count(*) from user_t where id = :id) > 0", nativeQuery = true)
-    fun checkUser(
-        @Param(value = "id")
-        userId: Int,
-    ): Boolean
-
-    @Query("select till_date from black_list where user_id = :user_id", nativeQuery = true)
-    fun getExpirationDateFromBlackList(
-        @Param(value = "user_id")
-        userId: Int,
-    ): LocalDateTime?
-
-    @Transactional
-    @Modifying
-    @Query(
-        "update user_t set num_of_grades = num_of_grades + 1, sum_of_grades = sum_of_grades + :grade " +
-                "where id = :user_id",
-        nativeQuery = true
-    )
-    fun addGrade(
-        @Param("user_id")
-        userId: Int,
-        @Param("grade")
-        grade: Int
-    ): Int
-
-    @Query(
-        "select sum_of_grades from user_t where id = :user_id",
-        nativeQuery = true
-    )
-    fun getSumOfGrades(
-        @Param("user_id")
-        userId: Int,
-    ): Int
-
-    @Query(
-        "select num_of_grades from user_t where id = :user_id",
-        nativeQuery = true
-    )
-    fun getNumOfGrades(
-        @Param("user_id")
-        userId: Int,
-    ): Int
-
 }
--- main/src/main/kotlin/com/ip13/main/security/service/UserService.kt
+++ main/src/main/kotlin/com/ip13/main/security/service/UserService.kt
@@ -7,7 +7,6 @@ import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
-import java.time.LocalDateTime
 
 @Service
 class UserService(
@@ -18,50 +17,26 @@ class UserService(
         return userRepository.findByUsername(username) ?: throw UsernameNotFoundException("No user with that name")
     }
 
-    fun findById(id: Int): User? {
+    fun save(user: User): User {
+        return userRepository.save(user)
+    }
+
+    fun findByIdOrNull(id: Int): User? {
         return userRepository.findByIdOrNull(id)
+    }
+
+    fun findAll(): List<User> {
+        return userRepository.findAll().toList()
     }
 
     fun existsByName(name: String): Boolean {
         return userRepository.existsByUsername(name)
     }
 
-    fun save(user: User): User {
-        return userRepository.save(user)
-    }
-
-    fun addRole(userId: Int, newRole: String): Boolean {
-        return userRepository.addRole(userId, newRole) == 1
-    }
-
-    fun addGrade(userId: Int, grade: Int): Int {
-        return userRepository.addGrade(userId, grade)
-    }
-
-    fun getSumOfGrades(userId: Int): Int {
-        return userRepository.getSumOfGrades(userId)
-    }
-
-    fun getNumOfGrades(userId: Int): Int {
-        return userRepository.getNumOfGrades(userId)
-    }
-
-    /**
-     * If user with id=userId doesn't exist in db throw exception, and controllerAdvice will return error 404
-     * Doesn't return value. TODO(Use as a checker)
-     */
     fun checkUser(userId: Int) {
-        if (!userRepository.checkUser(userId)) {
+        if (!userRepository.existsById(userId)) {
             throw UserNotFoundException("User with id: $userId not found")
         }
-    }
-
-    fun checkRole(userId: Int, role: String): Boolean {
-        return userRepository.checkRole(userId, role)
-    }
-
-    fun getExpirationDateFromBlackList(userId: Int): LocalDateTime? {
-        return userRepository.getExpirationDateFromBlackList(userId)
     }
 
     fun getUserByTokenInHeader(header: String): User {
