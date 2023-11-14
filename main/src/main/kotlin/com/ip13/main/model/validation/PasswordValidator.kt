ba536ef7cec97d1355fcc08aa6a26b0c3aaaf7e1
IP-13 <iperevozchikov2002@gmail.com>
2023-11-15 00:03:41 +0300

    feat(PasswordValidator): added PasswordValidator class for password validation

---
+++ main/src/main/kotlin/com/ip13/main/model/validation/PasswordValidator.kt
@@ -0,0 +1,28 @@
+package com.ip13.main.model.validation
+
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+
+class PasswordValidator : ConstraintValidator<Password, String> {
+    override fun isValid(value: String?, context: ConstraintValidatorContext?): Boolean {
+        if (value != null) {
+            val isValidLength = value.length in 5..10
+            val containsLowerCaseLetter = value.contains(Regex("[a-z]"))
+            val containsUpperCaseLetter = value.contains(Regex("[A-Z]"))
+            val containsDigit = value.contains(Regex("\\d"))
+            val containsSpecialSymbol = value.contains(Regex("\\W"))
+            val doesNotContainsWhitespaces = value.contains(Regex("\\S"))
+            if (isValidLength &&
+                containsLowerCaseLetter &&
+                containsUpperCaseLetter &&
+                containsDigit &&
+                containsSpecialSymbol &&
+                doesNotContainsWhitespaces
+            ) {
+                return true
+            }
+        }
+
+        return false
+    }
+}
