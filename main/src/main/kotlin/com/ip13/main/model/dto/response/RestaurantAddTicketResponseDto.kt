4b9ceb5f5c1705de2c948be7450d6136ccd8955c
Andrey Negrash <andi.ne@yandex.ru>
2023-11-15 06:29:23 +0300

    chore(RestaurantAddTicketResponseDto.kt): add validation annotations to all fields

--- main/src/main/kotlin/com/ip13/main/model/dto/response/RestaurantAddTicketResponseDto.kt
+++ main/src/main/kotlin/com/ip13/main/model/dto/response/RestaurantAddTicketResponseDto.kt
@@ -1,7 +1,9 @@
 package com.ip13.main.model.dto.response
 
 import com.ip13.main.model.enums.RestaurantAddStatus
+import jakarta.validation.constraints.NotNull
 
 data class RestaurantAddTicketResponseDto(
+    @NotNull
     val status: RestaurantAddStatus,
 )
