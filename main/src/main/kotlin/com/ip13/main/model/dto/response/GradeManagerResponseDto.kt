277d734b9fe63284cc9dfa696449239b509fb3e0
Andrey Negrash <andi.ne@yandex.ru>
2023-11-15 06:27:23 +0300

    chore(GradeManagerResponseDto.kt): add validation annotations to all fields

--- main/src/main/kotlin/com/ip13/main/model/dto/response/GradeManagerResponseDto.kt
+++ main/src/main/kotlin/com/ip13/main/model/dto/response/GradeManagerResponseDto.kt
@@ -1,5 +1,8 @@
 package com.ip13.main.model.dto.response
 
+import jakarta.validation.constraints.PositiveOrZero
+
 data class GradeManagerResponseDto(
+    @PositiveOrZero
     val newAverageGrade: Float,
 )
