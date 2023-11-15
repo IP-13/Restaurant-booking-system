b07b948d64cd6d382be5290cc878b37b7a1dfaf6
Andrey Negrash <andi.ne@yandex.ru>
2023-11-15 06:27:57 +0300

    chore(GradeVisitorResponseDto.kt): add validation annotations to all fields

--- main/src/main/kotlin/com/ip13/main/model/dto/response/GradeVisitorResponseDto.kt
+++ main/src/main/kotlin/com/ip13/main/model/dto/response/GradeVisitorResponseDto.kt
@@ -1,5 +1,8 @@
 package com.ip13.main.model.dto.response
 
+import jakarta.validation.constraints.PositiveOrZero
+
 data class GradeVisitorResponseDto(
+    @PositiveOrZero
     val newGrade: Float,
 )
