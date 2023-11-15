0a014dd2d80282b3b872ba475f74628c44bf2286
Andrey Negrash <andi.ne@yandex.ru>
2023-11-15 06:28:47 +0300

    chore(ReservationProcessResponseDto.kt): add validation annotations to all fields

--- main/src/main/kotlin/com/ip13/main/model/dto/response/ReservationProcessResponseDto.kt
+++ main/src/main/kotlin/com/ip13/main/model/dto/response/ReservationProcessResponseDto.kt
@@ -1,8 +1,12 @@
 package com.ip13.main.model.dto.response
 
 import com.ip13.main.model.enums.TableReserveStatus
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.PositiveOrZero
 
 data class ReservationProcessResponseDto(
+    @PositiveOrZero
     val id: Int,
+    @NotNull
     val status: TableReserveStatus,
 )
