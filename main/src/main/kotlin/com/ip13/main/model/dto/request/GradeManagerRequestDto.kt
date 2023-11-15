6dfd6b423689c63f8a90722444eb5bce4e1823fe
Andrey Negrash <andi.ne@yandex.ru>
2023-11-15 06:07:41 +0300

    chore(GradeManagerRequestDto.kt): add validation annotations to all fields

--- main/src/main/kotlin/com/ip13/main/model/dto/request/GradeManagerRequestDto.kt
+++ main/src/main/kotlin/com/ip13/main/model/dto/request/GradeManagerRequestDto.kt
@@ -1,7 +1,13 @@
 package com.ip13.main.model.dto.request
 
+import jakarta.validation.constraints.Positive
+import jakarta.validation.constraints.Size
+
 data class GradeManagerRequestDto(
+    @Positive
     val tableReserveTicketId: Int,
+    @Positive
     val grade: Int,
+    @Size(min = 3, max = 25, message = "Comment should be from 3 to 25 symbols")
     val comment: String?,
 )
