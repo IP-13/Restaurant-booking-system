8602ac1118aac83c4dbe92e6500b629d20684d54
Andrey Negrash <andi.ne@yandex.ru>
2023-11-15 06:23:38 +0300

    chore(RoleDeleteRequestDto.kt): add validation annotations to all fields

--- main/src/main/kotlin/com/ip13/main/model/dto/request/RoleDeleteRequestDto.kt
+++ main/src/main/kotlin/com/ip13/main/model/dto/request/RoleDeleteRequestDto.kt
@@ -1,8 +1,12 @@
 package com.ip13.main.model.dto.request
 
 import com.ip13.main.model.enums.Role
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.PositiveOrZero
 
 data class RoleDeleteRequestDto(
+    @PositiveOrZero
     val userId: Int,
+    @NotNull
     val role: Role,
 )
