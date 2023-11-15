31a336c703ef96de9cde4d7dd05fa5468380eeb2
Andrey Negrash <andi.ne@yandex.ru>
2023-11-15 07:25:47 +0300

    chore(ResponseDto.kt): add validation annotations to all fields

--- main/src/main/kotlin/com/ip13/main/security/dto/ResponseDto.kt
+++ main/src/main/kotlin/com/ip13/main/security/dto/ResponseDto.kt
@@ -1,9 +1,13 @@
 package com.ip13.main.security.dto
 
+import jakarta.validation.constraints.NotBlank
+
 data class RegisterResponseDto(
+    @NotBlank
     val token: String,
 )
 
 data class LoginResponseDto(
+    @NotBlank
     val token: String,
 )
