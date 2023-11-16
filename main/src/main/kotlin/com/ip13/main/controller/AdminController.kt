58a6695acd1b563dc3db19d0edbfdf01c728f568
IP-13 <iperevozchikov2002@gmail.com>
2023-11-16 22:45:13 +0300

    refactoring(AdminController): DeleteRoleResponse added instead ResponseEntity

--- main/src/main/kotlin/com/ip13/main/controller/AdminController.kt
+++ main/src/main/kotlin/com/ip13/main/controller/AdminController.kt
@@ -5,6 +5,7 @@ import com.ip13.main.model.dto.request.RoleAddRequest
 import com.ip13.main.model.dto.request.RoleDeleteRequest
 import com.ip13.main.model.dto.response.AddRoleResponse
 import com.ip13.main.model.dto.response.AddToBlackListResponse
+import com.ip13.main.model.dto.response.DeleteRoleResponse
 import com.ip13.main.security.service.UserService
 import com.ip13.main.service.BlackListService
 import com.ip13.main.util.getLogger
@@ -52,15 +53,15 @@ class AdminController(
         @Valid
         @RequestBody(required = true)
         request: RoleDeleteRequest,
-    ): ResponseEntity<String> {
+    ): DeleteRoleResponse {
         logger.debug("/admin/delete_role endpoint invoked")
 
         val isDeleted = userService.deleteRole(request)
 
         return if (isDeleted) {
-            ResponseEntity.ok("Role ${request.role} successfully deleted from user ${request.userId}")
+            DeleteRoleResponse("Role ${request.role} successfully deleted from user ${request.userId}")
         } else {
-            ResponseEntity.ok("User ${request.userId} does not have role ${request.role} to delete")
+            DeleteRoleResponse("User ${request.userId} does not have role ${request.role} to delete")
         }
     }
 
