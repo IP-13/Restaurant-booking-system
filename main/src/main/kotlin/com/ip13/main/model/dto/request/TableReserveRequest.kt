19f0110c3d0ba6dee348347112313b8a5114ec33
Andrey Negrash <andi.ne@yandex.ru>
2023-11-18 18:07:42 +0300

    fix(TableReserveRequest.kt): change @PositiveOrZero to @Positive and remove default parameters for values

--- main/src/main/kotlin/com/ip13/main/model/dto/request/TableReserveRequest.kt
+++ main/src/main/kotlin/com/ip13/main/model/dto/request/TableReserveRequest.kt
@@ -2,19 +2,19 @@ package com.ip13.main.model.dto.request
 
 import jakarta.validation.constraints.Future
 import jakarta.validation.constraints.FutureOrPresent
-import jakarta.validation.constraints.PositiveOrZero
+import jakarta.validation.constraints.Positive
 import jakarta.validation.constraints.Size
 import java.time.LocalDateTime
 
 data class TableReserveRequest(
-    @PositiveOrZero
-    val restaurantId: Int = 0,
+    @Positive
+    val restaurantId: Int,
     @FutureOrPresent
     val fromDate: LocalDateTime = LocalDateTime.now(),
     @Future
     val tillDate: LocalDateTime = LocalDateTime.now(),
-    @PositiveOrZero
-    val numOfGuests: Int = 0,
+    @Positive
+    val numOfGuests: Int,
     @Size(max = 25, message = "User comment length should be less than 25 symbols")
     val userComment: String? = null,
 )
