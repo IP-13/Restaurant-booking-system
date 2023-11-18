3b96fb71adfeef954ebc787a64d183e8ce61df22
Andrey Negrash <andi.ne@yandex.ru>
2023-11-18 18:13:48 +0300

    fix(TableReserveRequest.kt): delete all unused default values

--- main/src/main/kotlin/com/ip13/main/model/dto/request/TableReserveRequest.kt
+++ main/src/main/kotlin/com/ip13/main/model/dto/request/TableReserveRequest.kt
@@ -1,20 +1,17 @@
 package com.ip13.main.model.dto.request
 
-import jakarta.validation.constraints.Future
-import jakarta.validation.constraints.FutureOrPresent
-import jakarta.validation.constraints.Positive
-import jakarta.validation.constraints.Size
+import jakarta.validation.constraints.*
 import java.time.LocalDateTime
 
 data class TableReserveRequest(
-    @Positive
+    @PositiveOrZero
     val restaurantId: Int,
     @FutureOrPresent
-    val fromDate: LocalDateTime = LocalDateTime.now(),
+    val fromDate: LocalDateTime,
     @Future
-    val tillDate: LocalDateTime = LocalDateTime.now(),
+    val tillDate: LocalDateTime,
     @Positive
     val numOfGuests: Int,
     @Size(max = 25, message = "User comment length should be less than 25 symbols")
-    val userComment: String? = null,
+    val userComment: String?,
 )
