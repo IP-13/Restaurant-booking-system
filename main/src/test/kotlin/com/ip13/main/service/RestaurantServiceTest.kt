1b9004431d9bb0bc8419bd457310b18717f8e787
Michaelsladkov <msladkov2002@gmail.com>
2023-11-18 13:54:56 +0300

    test(restaurant service): added 4 test cases

---
+++ main/src/test/kotlin/com/ip13/main/service/RestaurantServiceTest.kt
@@ -0,0 +1,60 @@
+package com.ip13.main.service
+
+import com.ip13.main.exceptionHandling.exception.RestaurantNotFoundException
+import com.ip13.main.model.entity.Restaurant
+import com.ip13.main.repository.RestaurantRepository
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.data.repository.findByIdOrNull
+
+@ExtendWith(MockKExtension::class)
+class RestaurantServiceTest {
+    @MockK
+    private lateinit var restaurantRepository: RestaurantRepository
+
+    @InjectMockKs
+    private lateinit var restaurantService: RestaurantService
+
+    @Test
+    fun saveRestaurantTest() {
+        val defaultRestaurant = Restaurant()
+        every { restaurantRepository.save(defaultRestaurant) } returns defaultRestaurant
+        Assertions.assertEquals(defaultRestaurant, restaurantService.save(defaultRestaurant))
+    }
+
+    @Test
+    fun findByIdOrNullTest() {
+        val defaultRestaurant = Restaurant(id = TEST_RESTAURANT_ID)
+        every { restaurantRepository.findByIdOrNull(TEST_RESTAURANT_ID) } returns defaultRestaurant
+        every { restaurantRepository.findByIdOrNull(TEST_RESTAURANT_ID + 1) } returns null
+        Assertions.assertEquals(defaultRestaurant, restaurantService.findByIdOrNull(TEST_RESTAURANT_ID))
+        Assertions.assertNull(restaurantService.findByIdOrNull(TEST_RESTAURANT_ID + 1))
+    }
+
+    @Test
+    fun findByIdOrThrow() {
+        val defaultRestaurant = Restaurant(id = TEST_RESTAURANT_ID)
+        every { restaurantRepository.findByIdOrNull(TEST_RESTAURANT_ID) } returns defaultRestaurant
+        every { restaurantRepository.findByIdOrNull(TEST_RESTAURANT_ID + 1) } returns null
+        Assertions.assertEquals(defaultRestaurant, restaurantService.findByIdOrThrow(TEST_RESTAURANT_ID))
+        assertThrows<RestaurantNotFoundException> { restaurantService.findByIdOrThrow(TEST_RESTAURANT_ID + 1) }
+    }
+
+    @Test
+    fun findByManagerIdTest() {
+        val defaultRestaurant = Restaurant()
+        every { restaurantRepository.findByManagerId(TEST_MANAGER_ID) } returns defaultRestaurant
+        Assertions.assertEquals(defaultRestaurant, restaurantService.findByManagerId(TEST_MANAGER_ID))
+    }
+
+    companion object {
+        const val TEST_RESTAURANT_ID = 11
+        const val TEST_MANAGER_ID = 13
+    }
+}
