4e824647fa29e2d5b1d915be60e1446b72d7bbec
Andrey Negrash <andi.ne@yandex.ru>
2023-11-18 17:29:26 +0300

    fix(GradeRestaurantRequest.kt): change @Positive to @Min and @Max values

--- main/src/main/kotlin/com/ip13/main/model/dto/request/GradeRestaurantRequest.kt
+++ main/src/main/kotlin/com/ip13/main/model/dto/request/GradeRestaurantRequest.kt
@@ -1,12 +1,15 @@
 package com.ip13.main.model.dto.request
 
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.Positive
 import jakarta.validation.constraints.Size
 
 data class GradeRestaurantRequest(
     @Positive
     val tableReserveTicketId: Int,
-    @Positive
+    @Min(1)
+    @Max(5)
     val grade: Int,
     @Size(max = 25, message = "Comment length should be less than 25 symbols")
     val comment: String?,
