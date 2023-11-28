f7f2e78e1a4e00679b8588186244248ec21f002f
IP-13 <iperevozchikov2002@gmail.com>
2023-11-28 19:53:14 +0300

    refactoring: eureka-server renamed to eureka

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 rootProject.name = "Restaurant-booking-system"
-include("eureka-server")
+include("eureka")
 include("booking-system")
 include("gateway")
 include("config-server")
