5cbebdf92fe12c1ce34b80cce1679d9d3c2b9348
IP-13 <iperevozchikov2002@gmail.com>
2023-12-06 21:01:00 +0300

    fix: refreshScope added to WorkloadEmulator

--- user-service/src/main/kotlin/com/ip13/main/util/WorkloadEmulator.kt
+++ user-service/src/main/kotlin/com/ip13/main/util/WorkloadEmulator.kt
@@ -2,9 +2,11 @@ package com.ip13.main.util
 
 import jakarta.ws.rs.BadRequestException
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.stereotype.Component
 import kotlin.random.Random
 
+@RefreshScope
 @Component
 class WorkloadEmulator {
     private val log = getLogger(javaClass)
@@ -17,6 +19,8 @@ class WorkloadEmulator {
      * @throws BadRequestException
      */
     fun emulateWorkload() {
+        log.debug("fail-probability: {}", probability)
+
         if (probability.toDouble() > Random.nextDouble()) {
             log.debug("Workload is too high. Server cannot handle it")
             throw BadRequestException()
