c79f7781b9d766c4347189dc2521b50a9752bd2f
IP-13 <iperevozchikov2002@gmail.com>
2023-12-06 15:26:02 +0300

    feat: tableReserveTicket dto added to grade-service

---
+++ grade-service/src/main/kotlin/com/ip13/main/webClient/dto/TableReserveStatus.kt
@@ -0,0 +1,7 @@
+package com.ip13.main.webClient.dto
+
+enum class TableReserveStatus {
+    PROCESSING,
+    ACCEPTED,
+    REJECTED;
+}
---
+++ grade-service/src/main/kotlin/com/ip13/main/webClient/dto/TableReserveTicket.kt
@@ -0,0 +1,17 @@
+package com.ip13.main.webClient.dto
+
+import java.time.LocalDateTime
+
+data class TableReserveTicket(
+    val id: Int,
+    val restaurantId: Int,
+    val username: String,
+    val creationDate: LocalDateTime,
+    val fromDate: LocalDateTime,
+    val tillDate: LocalDateTime,
+    val numOfGuests: Int,
+    val userComment: String?,
+    val managerName: String?,
+    val managerComment: String?,
+    val status: TableReserveStatus,
+)
