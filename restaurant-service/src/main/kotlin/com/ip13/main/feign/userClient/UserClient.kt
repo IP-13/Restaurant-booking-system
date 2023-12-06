cf1613175f3e63628e6150523e0d07c35a577f5a
IP-13 <iperevozchikov2002@gmail.com>
2023-12-06 21:02:00 +0300

    feat: circuit-breaker configured

--- restaurant-service/src/main/kotlin/com/ip13/main/feign/userClient/UserClient.kt
+++ restaurant-service/src/main/kotlin/com/ip13/main/feign/userClient/UserClient.kt
@@ -2,9 +2,13 @@ package com.ip13.main.feign.userClient
 
 import com.ip13.main.feign.userClient.dto.RoleAddRequest
 import com.ip13.main.feign.userClient.dto.User
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
+import io.github.resilience4j.retry.annotation.Retry
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.*
 
+@Retry(name = "user-service-retry")
+@CircuitBreaker(name = "user-service-breaker")
 @FeignClient(name = "user-service")
 interface UserClient {
     @GetMapping("/auth/user/{username}")
