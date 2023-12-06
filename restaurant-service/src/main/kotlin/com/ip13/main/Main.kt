8db7aaca854035475aa3047492dd7df27adb8298
IP-13 <iperevozchikov2002@gmail.com>
2023-12-06 12:04:05 +0300

    feat: enable feign client in restaurant-service

--- restaurant-service/src/main/kotlin/com/ip13/main/Main.kt
+++ restaurant-service/src/main/kotlin/com/ip13/main/Main.kt
@@ -2,7 +2,9 @@ package com.ip13.main
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
+@EnableFeignClients
 @SpringBootApplication
 class Main
 
