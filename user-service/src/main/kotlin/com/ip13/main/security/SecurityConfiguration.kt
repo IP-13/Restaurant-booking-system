9c8abb03c4b7560a8edc47ba4b225cf11d06ca33
IP-13 <iperevozchikov2002@gmail.com>
2023-12-07 13:43:24 +0300

    feat: actuator endpoint configured for user-service

--- user-service/src/main/kotlin/com/ip13/main/security/SecurityConfiguration.kt
+++ user-service/src/main/kotlin/com/ip13/main/security/SecurityConfiguration.kt
@@ -27,6 +27,7 @@ class SecurityConfiguration(
                 authorize("/auth/role/**", hasAuthority(Role.ADMIN.name))
                 authorize("/auth/login/**", permitAll)
                 authorize("/auth/user/**", authenticated)
+                authorize("/user-actuator/**", hasAuthority(Role.ADMIN.name))
                 authorize(anyRequest, authenticated)
             }
             sessionManagement {
