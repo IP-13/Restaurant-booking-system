2c5e204f3a6f9595e2b7be5a93ee3eb326fefa56
IP-13 <iperevozchikov2002@gmail.com>
2023-12-08 19:32:09 +0300

    fix: table anno added to user entity in grade-service

--- grade-service/src/main/kotlin/com/ip13/main/model/entity/User.kt
+++ grade-service/src/main/kotlin/com/ip13/main/model/entity/User.kt
@@ -1,7 +1,9 @@
 package com.ip13.main.model.entity
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
 
+@Table("user_t")
 data class User(
     @Id
     val username: String,
