1db98f34b20eb8938ee35b60a6bb982e2e2371e2
IP-13 <iperevozchikov2002@gmail.com>
2023-12-09 13:22:13 +0300

    feat: VisitorGradeHandler

--- grade-service/src/main/kotlin/com/ip13/main/handler/VisitorGradeHandler.kt
+++ grade-service/src/main/kotlin/com/ip13/main/handler/VisitorGradeHandler.kt
@@ -6,11 +6,9 @@ import com.ip13.main.model.entity.User
 import com.ip13.main.model.entity.VisitorGrade
 import com.ip13.main.repository.RestaurantGradeRepository
 import com.ip13.main.repository.RestaurantRepository
-import com.ip13.main.repository.UserRepository
 import com.ip13.main.repository.VisitorGradeRepository
 import com.ip13.main.util.getLogger
 import com.ip13.main.webClient.restaurantService.RestaurantServiceWebClient
-import com.ip13.main.webClient.restaurantService.dto.TableReserveTicket
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
@@ -20,25 +18,24 @@ import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Mono
 
 @Component
-class GradeHandler(
+class VisitorGradeHandler(
     private val userServiceClient: WebClient,
     private val restaurantServiceWebClient: RestaurantServiceWebClient,
     private val restaurantGradeRepository: RestaurantGradeRepository,
     private val restaurantRepository: RestaurantRepository,
-    private val userRepository: UserRepository,
+    private val userHandler: UserHandler,
     private val visitorGradeRepository: VisitorGradeRepository,
 ) {
     private val log = getLogger(javaClass)
 
-    fun gradeUser(request: ServerRequest): Mono<ServerResponse> {
-        log.debug("in grade user method")
+    fun gradeVisitor(request: ServerRequest): Mono<ServerResponse> {
+        log.debug("in grade visitor method")
         return ServerResponse
             .ok()
             .contentType(MediaType.APPLICATION_JSON)
             .body(
                 request
                     .bodyToMono(RestaurantGradeRequest::class.java)
-                    .log()
                     .flatMap { req ->
                         restaurantServiceWebClient
                             .getTableReserveTicket(
@@ -47,58 +44,74 @@ class GradeHandler(
                             )
                             .log()
                             .flatMap { ticket ->
-                                log.debug("Before user repository")
-                                userRepository.findByUsername(ticket.username)
-                                    .flatMap {
-                                        log.debug("user found")
-                                        request.principal().flatMap { principal ->
-                                            visitorGradeRepository.save(
-                                                VisitorGrade(
-                                                    managerName = principal.name,
-                                                    tableReserveTicketId = req.tableReserveTicketId,
-                                                    username = ticket.username,
-                                                    grade = req.grade,
-                                                    comment = req.comment,
+                                // save manager
+                                log.debug("saving manager")
+                                request.principal().flatMap { principal ->
+                                    userHandler.findByUsername(principal.name)
+                                        .switchIfEmpty(
+                                            userHandler.save(
+                                                User(
+                                                    username = principal.name,
+                                                    numOfGrades = 0,
+                                                    sumOfGrades = 0,
                                                 )
-                                            ).log()
-                                        }
-                                        userRepository.addGrade(ticket.username, req.grade)
-                                    }
-                                    .log()
-                                    .switchIfEmpty(
-                                        userRepository.save(
-                                            User(
-                                                username = ticket.username,
-                                                numOfGrades = 1,
-                                                sumOfGrades = req.grade
                                             )
-                                        ).flatMap {
-                                            log.debug("user not found")
+                                        )
+                                }.flatMap {
+                                    log.debug("Checking if user exists")
+                                    userHandler.findByUsername(ticket.username)
+                                        .flatMap {
+                                            log.debug("User found. Adding grade")
+                                            userHandler.addGrade(ticket.username, req.grade)
+                                        }
+                                        .switchIfEmpty(
+                                            userHandler.save(
+                                                User(
+                                                    username = ticket.username,
+                                                    numOfGrades = 1,
+                                                    sumOfGrades = req.grade
+                                                )
+                                            ).flatMap {
+                                                log.debug("User not found. Saving user")
+                                                Mono.just(it.sumOfGrades)
+                                            }
+                                        )
+                                        .flatMap {
+                                            log.debug("Saving visitor grade to db")
                                             request.principal().flatMap { principal ->
-                                                log.debug("saving visitor grade")
                                                 visitorGradeRepository.save(
                                                     VisitorGrade(
-                                                        managerName = principal.name, // add manager as well
+                                                        managerName = principal.name,
                                                         tableReserveTicketId = req.tableReserveTicketId,
                                                         username = ticket.username,
                                                         grade = req.grade,
                                                         comment = req.comment,
                                                     )
                                                 ).log()
+                                            }.flatMap {
+                                                log.debug("Getting updated grade from db")
+                                                userHandler.getGrade(ticket.username)
                                             }
-                                            log.debug("num of grades: {}", it.numOfGrades)
-                                            log.debug("sum of grades: {}", it.sumOfGrades)
-                                            Mono.just(it.sumOfGrades)
                                         }
-                                    ).log().flatMap {
-                                        userRepository.getGrade(ticket.username)
-                                    }
+                                }
                             }
+                            .switchIfEmpty(Mono.error(TableReserveTicketNotFound()))
                             .log()
-                            .switchIfEmpty(Mono.error(TableReserveTicketNotFound())).log()
                     }
                     .log(),
                 Float::class.java
             )
     }
+
+    fun getGrade(request: ServerRequest): Mono<ServerResponse> {
+        log.debug("in grade visitor method")
+        return ServerResponse
+            .ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(
+                userHandler.findByUsername(request.pathVariable("username")),
+                Float::class.java
+            )
+    }
+    
 }
