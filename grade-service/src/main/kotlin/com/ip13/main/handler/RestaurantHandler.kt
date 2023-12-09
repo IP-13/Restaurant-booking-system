18eaadcc1e4686f79c4b7301ab865f47c14ed329
IP-13 <iperevozchikov2002@gmail.com>
2023-12-09 15:55:12 +0300

    feat: RestaurantHandler added

---
+++ grade-service/src/main/kotlin/com/ip13/main/handler/RestaurantHandler.kt
@@ -0,0 +1,25 @@
+package com.ip13.main.handler
+
+import com.ip13.main.model.entity.Restaurant
+import com.ip13.main.repository.RestaurantRepository
+import reactor.core.publisher.Mono
+
+class RestaurantHandler(
+    private val restaurantRepository: RestaurantRepository,
+) {
+    fun findById(id: Int): Mono<Restaurant> {
+        return restaurantRepository.findById(id)
+    }
+
+    fun addGrade(id: Int, grade: Int): Mono<Int> {
+        return restaurantRepository.addGrade(id, grade)
+    }
+
+    fun save(restaurant: Restaurant): Mono<Restaurant> {
+        return restaurantRepository.save(restaurant)
+    }
+
+    fun getGrade(id: Int): Mono<Float> {
+        return restaurantRepository.getGrade(id)
+    }
+}
