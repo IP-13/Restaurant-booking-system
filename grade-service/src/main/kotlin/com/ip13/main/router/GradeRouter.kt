317225ded5a8d9f38534864fa99b5582ea14598d
IP-13 <iperevozchikov2002@gmail.com>
2023-12-09 16:14:17 +0300

    feat: gradeRouter configuration

--- grade-service/src/main/kotlin/com/ip13/main/router/GradeRouter.kt
+++ grade-service/src/main/kotlin/com/ip13/main/router/GradeRouter.kt
@@ -1,5 +1,6 @@
 package com.ip13.main.router
 
+import com.ip13.main.handler.RestaurantGradeHandler
 import com.ip13.main.handler.VisitorGradeHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -13,13 +14,23 @@ import org.springframework.web.reactive.function.server.ServerResponse
 @Configuration
 class GradeRouter {
     @Bean
-    fun routes(handler: VisitorGradeHandler): RouterFunction<ServerResponse> {
+    fun routes(
+        visitorGradeHandler: VisitorGradeHandler,
+        restaurantGradeHandler: RestaurantGradeHandler,
+    ): RouterFunction<ServerResponse> {
         return nest(
-            path("/grade/visitor"),
+            accept(MediaType.APPLICATION_JSON),
             nest(
-                (accept(MediaType.APPLICATION_JSON)),
-                route(POST(""), handler::gradeVisitor)
-                    .andRoute(GET("/{username}"), handler::getGrade)
+                path("/grade"),
+                nest(
+                    path("/visitor"),
+                    route(POST(""), visitorGradeHandler::gradeVisitor)
+                        .andRoute(GET("/{username}"), visitorGradeHandler::getGrade)
+                ).andNest(
+                    path("/restaurant"),
+                    route(POST(""), restaurantGradeHandler::gradeRestaurant)
+                        .andRoute(GET("{id}"), restaurantGradeHandler::getGrade)
+                )
             )
         )
     }
