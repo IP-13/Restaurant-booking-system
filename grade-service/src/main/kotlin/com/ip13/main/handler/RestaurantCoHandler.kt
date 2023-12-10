a554627f3590c2deaf404bd4206a4ea31e085d81
IP-13 <iperevozchikov2002@gmail.com>
2023-12-10 16:25:03 +0300

    feat(grade-service): non-blocking api with coroutines for restaurant added

---
+++ grade-service/src/main/kotlin/com/ip13/main/handler/RestaurantCoHandler.kt
@@ -0,0 +1,26 @@
+package com.ip13.main.handler
+
+import com.ip13.main.model.entity.Restaurant
+import com.ip13.main.repository.RestaurantCoRepository
+import org.springframework.stereotype.Component
+
+@Component
+class RestaurantCoHandler(
+    private val restaurantCoRepository: RestaurantCoRepository,
+) {
+    suspend fun findByRestaurantIdOrNull(restaurantId: Int): Restaurant? {
+        return restaurantCoRepository.findByRestaurantIdOrNull(restaurantId)
+    }
+
+    suspend fun addGrade(id: Int, grade: Int): Int {
+        return restaurantCoRepository.addGrade(id, grade)
+    }
+
+    suspend fun save(restaurant: Restaurant): Restaurant {
+        return restaurantCoRepository.save(restaurant)
+    }
+
+    suspend fun getGrade(id: Int): Float {
+        return restaurantCoRepository.getGrade(id)
+    }
+}
