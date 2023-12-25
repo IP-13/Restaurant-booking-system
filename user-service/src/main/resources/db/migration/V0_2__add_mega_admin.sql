8c9259daddb5aa249f99bc00dbb715a5ca1277c3
IP-13 <iperevozchikov2002@gmail.com>
2023-12-25 19:04:09 +0300

    mega fix 31

--- user-service/src/main/resources/db/migration/V0_2__add_mega_admin.sql
+++ user-service/src/main/resources/db/migration/V0_2__add_mega_admin.sql
@@ -1,2 +1,2 @@
 insert into user_t (username, password, roles)
-values ('mega_admin', '$2a$10$74v.SgHdDyr0fX.JbtK7G.TTJO35tyJoK7C3          port: 22xXyHxQ0MbSm2BrnO6', '{"ADMIN", "MANAGER"}');
+values ('mega_admin', '$2a$10$74v.SgHdDyr0fX.JbtK7G.TTJO35tyJoK7C3xXyHxQ0MbSm2BrnO6', '{"ADMIN", "MANAGER"}');
