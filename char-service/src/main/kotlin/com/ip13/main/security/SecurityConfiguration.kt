c3f9222ca8a341b7ef73d8f9078597a14f669576
IP-13 <iperevozchikov2002@gmail.com>
2024-01-06 16:53:00 +0300

    chat-service default configuration

---
+++ char-service/src/main/kotlin/com/ip13/main/security/SecurityConfiguration.kt
@@ -0,0 +1,57 @@
+package com.ip13.main.security
+
+import com.ip13.main.security.model.enum.Role
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration(
+    val jwtRequestFilter: JwtRequestFilter,
+    val authenticationConfiguration: AuthenticationConfiguration,
+) {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            authorizeHttpRequests {
+                authorize("/restaurant/create-ticket", authenticated)
+                authorize("/restaurant/process-ticket", hasAuthority(Role.ADMIN.name))
+                authorize("/restaurant/id/**", authenticated)
+                authorize("/reservation/reserve-table", authenticated)
+                authorize("/reservation/process-reservation", hasAuthority(Role.MANAGER.name))
+                authorize("/reservation/add-booking-constraint", hasAuthority(Role.MANAGER.name))
+                authorize("/reservation/table-reserve-ticket/**", authenticated)
+                authorize("/swagger-ui/**", permitAll)
+                authorize("/v3/api-docs/**", permitAll)
+            }
+            sessionManagement {
+                sessionCreationPolicy = SessionCreationPolicy.STATELESS
+            }
+            csrf {
+                disable()
+            }
+            addFilterBefore<UsernamePasswordAuthenticationFilter>(jwtRequestFilter)
+        }
+
+        return http.build()
+    }
+
+    @Bean
+    fun authenticationManager(): AuthenticationManager {
+        return authenticationConfiguration.getAuthenticationManager()
+    }
+
+    @Bean
+    fun passwordEncoder(): BCryptPasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+}
---
+++ char-service/src/main/kotlin/com/ip13/main/security/model/enum/Role.kt
@@ -0,0 +1,6 @@
+package com.ip13.main.security.model.enum
+
+enum class Role {
+    MANAGER,
+    ADMIN;
+}
