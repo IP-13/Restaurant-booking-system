e866a2f57855e77750e393390814658d91347f1d
IP-13 <iperevozchikov2002@gmail.com>
2024-01-22 10:29:43 +0300

    feat(chat-service): kubernetes profile added and kubernetes discovery added

--- chat-service/src/main/kotlin/com/ip13/main/Main.kt
+++ chat-service/src/main/kotlin/com/ip13/main/Main.kt
@@ -2,7 +2,9 @@ package com.ip13.main
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
+@EnableDiscoveryClient
 @SpringBootApplication
 class Main
 
