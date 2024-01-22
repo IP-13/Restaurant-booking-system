4b5462de756652e5168936b59270e6de4e4f40a0
IP-13 <iperevozchikov2002@gmail.com>
2024-01-22 19:31:11 +0300

    feat(user-service): restaurant created event added

--- grade-service/src/main/kotlin/com/ip13/main/kafka/RegistrationListener.kt
+++ grade-service/src/main/kotlin/com/ip13/main/kafka/RegistrationListener.kt
@@ -4,7 +4,9 @@ import com.ip13.main.event.RegistrationEvent
 import com.ip13.main.handler.UserHandler
 import com.ip13.main.model.entity.User
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Service
 
+@Service
 class RegistrationListener(
     private val userHandler: UserHandler,
 ) {
---
+++ user-service/src/main/kotlin/com/ip13/main/kafka/RestaurantCreatedListener.kt
@@ -0,0 +1,22 @@
+package com.ip13.main.kafka
+
+import com.ip13.main.event.RestaurantCreatedEvent
+import com.ip13.main.model.dto.request.RoleAddRequest
+import com.ip13.main.model.enums.Role
+import com.ip13.main.service.UserService
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Service
+
+@Service
+class RestaurantCreatedListener(
+    private val userService: UserService,
+) {
+    @KafkaListener(topics = [RESTAURANT_CREATED_TOPIC])
+    fun addUser(event: RestaurantCreatedEvent) {
+        userService.addRole(RoleAddRequest(username = event.managerName, role = Role.MANAGER))
+    }
+
+    companion object {
+        private const val RESTAURANT_CREATED_TOPIC = "restaurant-created"
+    }
+}
